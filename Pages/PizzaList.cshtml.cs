3b076112776ae678b7994aca55222380c96c6673
MarcosRehtanz <marcos.rehtanz@gmail.com>
2023-12-22 15:20:09 -0300

    feat: add controller to post a new pizza

--- Pages/PizzaList.cshtml.cs
+++ Pages/PizzaList.cshtml.cs
@@ -10,9 +10,29 @@ namespace ContosoPizza.Pages
         private readonly PizzaService _service;
         public IList<Pizza> PizzaList { get; set; } = default!;
 
+        /*
+            ? [BindProperty] se utiliza para enlazar
+            ?   la propiedad NewPizza a la página de Razor.
+            ? Al realizarse una solicitud HTTP POST,
+            ?   la propiedad NewPizza se rellenará
+            ?   con la entrada del usuario.
+        */
+        [BindProperty]
+        public Pizza NewPizza { get; set; } = default!;
+
         public PizzaListModel(PizzaService service)
         {
             _service = service;
+        }
+
+        public IActionResult OnPost()
+        {
+            if(!ModelState.IsValid || NewPizza == null)
+            {
+                return Page();
+            }
+            _service.AddPizza(NewPizza);
+            return RedirectToAction("Get");
         }
 
         public void OnGet()
